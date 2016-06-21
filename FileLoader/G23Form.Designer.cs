33133228601e6621729eb1ef54b7f062addfd529
Ivyhill Technologies <ivyhill-bizspark2@outlook.com>
2016-06-21 01:43:00 -0400

    g23 excel browse progress updates

--- FileLoader/G23Form.Designer.cs
+++ FileLoader/G23Form.Designer.cs
@@ -29,19 +29,31 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(G23Form));
-            this.radTextBoxControl1 = new Telerik.WinControls.UI.RadTextBoxControl();
+            Telerik.WinControls.UI.TableViewDefinition tableViewDefinition1 = new Telerik.WinControls.UI.TableViewDefinition();
+            this.txtFilePath = new Telerik.WinControls.UI.RadTextBoxControl();
             this.label1 = new System.Windows.Forms.Label();
             this.usersRadDropDownList = new Telerik.WinControls.UI.RadDropDownList();
-            ((System.ComponentModel.ISupportInitialize)(this.radTextBoxControl1)).BeginInit();
+            this.btn_Browse = new System.Windows.Forms.Button();
+            this.btnUploadFile = new System.Windows.Forms.Button();
+            this.progressIndicatorBrowse = new System.Windows.Forms.PictureBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.dateUploaded = new Telerik.WinControls.UI.RadDateTimePicker();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.radGrid = new Telerik.WinControls.UI.RadGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFilePath)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.usersRadDropDownList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressIndicatorBrowse)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateUploaded)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGrid.MasterTemplate)).BeginInit();
             this.SuspendLayout();
             // 
-            // radTextBoxControl1
+            // txtFilePath
             // 
-            this.radTextBoxControl1.Location = new System.Drawing.Point(70, 70);
-            this.radTextBoxControl1.Name = "radTextBoxControl1";
-            this.radTextBoxControl1.Size = new System.Drawing.Size(500, 30);
-            this.radTextBoxControl1.TabIndex = 0;
+            this.txtFilePath.Location = new System.Drawing.Point(70, 70);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(500, 30);
+            this.txtFilePath.TabIndex = 0;
             // 
             // label1
             // 
@@ -61,20 +73,112 @@
             this.usersRadDropDownList.Size = new System.Drawing.Size(250, 30);
             this.usersRadDropDownList.TabIndex = 2;
             // 
+            // btn_Browse
+            // 
+            this.btn_Browse.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_Browse.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Browse.Location = new System.Drawing.Point(630, 70);
+            this.btn_Browse.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Browse.Name = "btn_Browse";
+            this.btn_Browse.Size = new System.Drawing.Size(200, 50);
+            this.btn_Browse.TabIndex = 22;
+            this.btn_Browse.Text = "Browse";
+            this.btn_Browse.UseVisualStyleBackColor = false;
+            this.btn_Browse.Click += new System.EventHandler(this.btn_Browse_Click);
+            // 
+            // btnUploadFile
+            // 
+            this.btnUploadFile.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnUploadFile.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUploadFile.Location = new System.Drawing.Point(70, 550);
+            this.btnUploadFile.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUploadFile.Name = "btnUploadFile";
+            this.btnUploadFile.Size = new System.Drawing.Size(200, 50);
+            this.btnUploadFile.TabIndex = 23;
+            this.btnUploadFile.Text = "Upload File";
+            this.btnUploadFile.UseVisualStyleBackColor = false;
+            // 
+            // progressIndicatorBrowse
+            // 
+            this.progressIndicatorBrowse.Image = ((System.Drawing.Image)(resources.GetObject("progressIndicatorBrowse.Image")));
+            this.progressIndicatorBrowse.Location = new System.Drawing.Point(112, 110);
+            this.progressIndicatorBrowse.Name = "progressIndicatorBrowse";
+            this.progressIndicatorBrowse.Size = new System.Drawing.Size(65, 65);
+            this.progressIndicatorBrowse.TabIndex = 30;
+            this.progressIndicatorBrowse.TabStop = false;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(70, 625);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 20);
+            this.lblMessage.TabIndex = 25;
+            // 
+            // dateUploaded
+            // 
+            this.dateUploaded.AutoSize = false;
+            this.dateUploaded.Font = new System.Drawing.Font("Calibri", 10F);
+            this.dateUploaded.Location = new System.Drawing.Point(255, 430);
+            this.dateUploaded.Margin = new System.Windows.Forms.Padding(4);
+            this.dateUploaded.Name = "dateUploaded";
+            this.dateUploaded.Size = new System.Drawing.Size(250, 30);
+            this.dateUploaded.TabIndex = 31;
+            this.dateUploaded.TabStop = false;
+            this.dateUploaded.Text = "Thursday, July 16, 2015";
+            this.dateUploaded.Value = new System.DateTime(2015, 7, 16, 17, 8, 1, 824);
+            // 
+            // lblDate
+            // 
+            this.lblDate.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(70, 440);
+            this.lblDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(100, 30);
+            this.lblDate.TabIndex = 32;
+            this.lblDate.Text = "Date Uploaded";
+            // 
+            // radGrid
+            // 
+            this.radGrid.Font = new System.Drawing.Font("Calibri", 10F);
+            this.radGrid.Location = new System.Drawing.Point(70, 180);
+            this.radGrid.Margin = new System.Windows.Forms.Padding(4);
+            // 
+            // 
+            // 
+            this.radGrid.MasterTemplate.AllowAddNewRow = false;
+            this.radGrid.MasterTemplate.ViewDefinition = tableViewDefinition1;
+            this.radGrid.Name = "radGrid";
+            this.radGrid.ReadOnly = true;
+            this.radGrid.Size = new System.Drawing.Size(1000, 150);
+            this.radGrid.TabIndex = 33;
+            this.radGrid.Text = "Data Grid";
+            // 
             // G23Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1192, 773);
+            this.Controls.Add(this.radGrid);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.dateUploaded);
+            this.Controls.Add(this.progressIndicatorBrowse);
+            this.Controls.Add(this.btnUploadFile);
+            this.Controls.Add(this.btn_Browse);
             this.Controls.Add(this.usersRadDropDownList);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.radTextBoxControl1);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.lblMessage);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "G23Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Office 365 IPO G-23 Uploader";
-            ((System.ComponentModel.ISupportInitialize)(this.radTextBoxControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtFilePath)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.usersRadDropDownList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressIndicatorBrowse)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dateUploaded)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGrid.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,8 +186,15 @@
 
         #endregion
 
-        private Telerik.WinControls.UI.RadTextBoxControl radTextBoxControl1;
+        private Telerik.WinControls.UI.RadTextBoxControl txtFilePath;
         private System.Windows.Forms.Label label1;
         private Telerik.WinControls.UI.RadDropDownList usersRadDropDownList;
+        private System.Windows.Forms.Button btn_Browse;
+        private System.Windows.Forms.Button btnUploadFile;
+        private System.Windows.Forms.PictureBox progressIndicatorBrowse;
+        private System.Windows.Forms.Label lblMessage;
+        private Telerik.WinControls.UI.RadDateTimePicker dateUploaded;
+        private System.Windows.Forms.Label lblDate;
+        private Telerik.WinControls.UI.RadGridView radGrid;
     }
 }
--- FileLoader/G23Form.cs
+++ FileLoader/G23Form.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.SharePoint.Client;
+﻿using Microsoft.Office.Interop.Excel;
+using Microsoft.SharePoint.Client;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,6 +12,10 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using FileLoader.Helpers;
 using System.Configuration;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+using Telerik.WinControls.UI;
 
 
 namespace FileLoader
@@ -31,7 +36,15 @@ namespace FileLoader
             InitializeComponent();
             this.OUser = user;
 
+            dateUploaded.Text = System.DateTime.Now.ToShortDateString();
+            //this.progressIndicator.Image = null;
+            this.progressIndicatorBrowse.Image = null;
+
             InitializeDynamicUserComboBox();
+
+            //Initialize GridView         
+            fieldMappings = ParseXML();
+            InitializeDataGridView(fieldMappings);
         }
 
 
@@ -72,6 +85,148 @@ namespace FileLoader
             }
 
         }
+
+        /// <summary>
+        /// Initializes Data GridView
+        /// </summary>
+        /// <param name="mappings"></param>
+        private void InitializeDataGridView(List<Mapping> mappings)
+        {
+            radGrid.AutoGenerateColumns = false;
+            radGrid.ShowColumnHeaders = true;
+            DataGridViewCellStyle columnHeaderStyle = new DataGridViewCellStyle();
+            columnHeaderStyle.BackColor = Color.Beige;
+            columnHeaderStyle.Font = new System.Drawing.Font("Verdana", 10, FontStyle.Bold);
+            //radGrid.MasterTemplate.AutoSizeColumnsMode = GridViewAutoSizeColumnsMode.Fill;
+
+            for (int i = 0; i < mappings.Count; i++)
+            {
+
+                GridViewTextBoxColumn textBoxColumn = new GridViewTextBoxColumn();
+                textBoxColumn.Name = mappings[i].SharePointListField;
+                textBoxColumn.HeaderText = mappings[i].ExcelColumnName;
+                textBoxColumn.FieldName = mappings[i].RangeName;
+                textBoxColumn.MaxLength = 50;
+                textBoxColumn.TextAlignment = ContentAlignment.BottomRight;
+                textBoxColumn.Width = 250;
+                radGrid.MasterTemplate.Columns.Add(textBoxColumn);
+
+            }
+        }
+
+
         #endregion
+
+        OpenFileDialog openFile = new OpenFileDialog();
+        private void btn_Browse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblMessage.Text = "";
+                btn_Browse.Enabled = false;
+                btnUploadFile.Enabled = false;
+
+                openFile.DefaultExt = ".xlsx";
+                openFile.Filter = "(.xlsx)|*.xlsx";
+                if (openFile.ShowDialog() == DialogResult.OK)
+                {
+
+                    //progressBar.Visible = true;
+                    //progressBar.Style = ProgressBarStyle.Marquee;
+                    //System.Threading.Thread thread = new System.Threading.Thread(new System.Threading.ThreadStart(LoadData(excelSheet)));
+                    //thread.Start();
+
+                    this.progressIndicatorBrowse.Image = Properties.Resources.ProgressBrowse;
+
+                    txtFilePath.Text = openFile.FileName;
+                    Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+                    Microsoft.Office.Interop.Excel.Workbook excelBook = excelApp.Workbooks.Open(txtFilePath.Text.ToString(), 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                    //Microsoft.Office.Interop.Excel.Worksheet excelSheet = (Microsoft.Office.Interop.Excel.Worksheet)excelBook.Worksheets.get_Item(Convert.ToInt32(ConfigurationManager.AppSettings["SpreadSheetIndex"])); ;
+                    //LoadData(excelSheet);
+
+                    List<Name> namedRanges = excelBook.GetNamedRanges();
+                    LoadData(namedRanges);
+
+                    //Dispose Excel Objects                  
+                    excelBook.Close(0);
+                    excelApp.Quit();
+
+                    btn_Browse.Enabled = true;
+                    btnUploadFile.Enabled = true;
+                    this.progressIndicatorBrowse.Image = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                this.progressIndicatorBrowse.Image = null;
+                lblMessage.Text = "The File you are trying to upload is not an Excel File, please try again." + System.Environment.NewLine + "Exception Details: " + System.Environment.NewLine + ex.Message;
+                lblMessage.ForeColor = System.Drawing.Color.Red;
+                lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10);
+                btn_Browse.Enabled = true;
+                btnUploadFile.Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// Loads Excel Data from Excel Workbook Named Ranges
+        /// </summary>
+        /// <param name="namedRange"></param>
+        public void LoadData(List<Name> namedRange)
+        {
+            var mappedField = string.Empty;
+            string strCellData = "";
+            int colCnt = 0;
+            System.Data.DataTable dt = new System.Data.DataTable();
+
+            for (colCnt = 0; colCnt <= namedRange.Count - 1; colCnt++)
+            {
+                string strColumn = "";
+                strColumn = Convert.ToString(namedRange[colCnt].Name);
+                dt.Columns.Add(strColumn, typeof(string));
+            }
+
+            string strData = "";
+            for (colCnt = 0; colCnt <= namedRange.Count - 1; colCnt++)
+            {
+                try
+                {
+                    strCellData = Convert.ToString(namedRange[colCnt].RefersToRange.Value2);
+                    strData += strCellData + "|";
+                }
+                catch (Exception ex)
+                {
+                    throw new FormatException("Invalid Excel Format Exception " + ex.Message);
+                }
+            }
+            strData = strData.Remove(strData.Length - 1, 1);
+            dt.Rows.Add(strData.Split('|'));
+
+            radGrid.DataSource = dt.DefaultView;
+        }
+
+        /// <summary>
+        /// Parses Field Mapping XML into List of Mapping
+        /// </summary>
+        /// <returns></returns>
+        public static List<Mapping> ParseXML()
+        {
+            FieldSetMapping fieldsetMapping;
+            List<FieldSet> fieldSets = new List<FieldSet>();
+            List<Mapping> mappings = new List<Mapping>();
+
+            var serializer = new XmlSerializer(typeof(FieldSetMapping));
+            using (var stream = new StreamReader("FieldSetsG23.xml"))
+            using (var reader = XmlReader.Create(stream))
+            {
+                fieldsetMapping = (FieldSetMapping)serializer.Deserialize(reader);
+            }
+
+            fieldSets = fieldsetMapping.FieldSets;
+            foreach (FieldSet f in fieldSets)
+            {
+                mappings = f.Mappings;
+            }
+            return mappings;
+        }
     }
 }
